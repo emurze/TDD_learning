aa43363495b6668b9596de9682cb1a71d56bb671
emurze <rozanov7475@yandex.by>
2023-10-28 21:20:54 +0300

    created first unittest

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -1,5 +1,15 @@
+"""
+Check main page title. It should work.
+Additionally, It should contain successfully.
+
+1. Client can't see the site
+
+End of this User History
+
+"""
+
 import logging
-from pprint import pprint
+import unittest
 
 from selenium import webdriver
 
@@ -8,9 +18,21 @@ lg = logging.getLogger(__name__)
 PROTOCOL = 'http'
 SOCKET = '0.0.0.0:8080'
 
-with webdriver.Firefox() as browser:
-    browser.get(f'{PROTOCOL}://{SOCKET}')
 
-    assert 'successfully' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
 
-    pprint(help(browser))
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get(f'{PROTOCOL}://{SOCKET}')
+
+        self.assertIn('install', self.browser.title)
+
+        self.fail(f'Browser title was {self.browser.title}')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
